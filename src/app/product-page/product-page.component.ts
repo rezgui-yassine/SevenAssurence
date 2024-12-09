4d7f5308369f897f5386ea85033088e6cc413643
Rezgui Yassine <rezguiyassine224@gmail.com>
2024-12-09 17:43:26 +0100

    add login modal for every product

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent {
+  isModalOpen = false;
 
+  // Function to open the modal
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  // Function to close the modal
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
