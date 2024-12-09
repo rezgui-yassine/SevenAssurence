fffa0c46c9e1478af1724c880918054e84bf0daf
Rezgui Yassine <rezguiyassine224@gmail.com>
2024-12-09 16:09:38 +0100

    my five commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsParentComponent } from './about-us-parent/about-us-parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Corrected default path
+  { path: 'aboutUsParent', component: AboutUsParentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
